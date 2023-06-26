9e1afe345147cdf3dc67b553492e6d0501a12cd2
Dinirubanuka <2021cs021@stu.ucsc.cmb.ac.lk>
2023-06-26 11:22:23 +0530

    labsheet3-21000212

---
+++ 21000212.js
@@ -0,0 +1,35 @@
+// --------01--------
+// function concatstrings(str1,str2){
+//     var str = str1.concat(str2);
+//     document.write(str);
+// }
+// concatstrings("Programmo","Phobia");
+
+// --------2.(a)---------
+// function concatstrings(str1){
+//     var str = str1.toLowerCase();
+//     document.write(str);
+// }
+// concatstrings("ProgrammoPhobia");
+
+// ---------2.(b)---------
+// function concatstrings(str1){
+//     var str = str1.toUpperCase();
+//     document.write(str);
+// }
+// concatstrings("ProgrammoPhobia");
+
+//-----------2.(c)----------
+// function splitstring(str1){
+//     var str = str1.split(",");
+//     console.log(str);
+// }
+// splitstring("Programmo ,Phobia");
+
+// ------------2.(d)----------
+// function index(str){
+//     var n = str.indexOf("Phobia");
+//     document.write(n);
+// }
+// index("Programmo Phobia");
+
